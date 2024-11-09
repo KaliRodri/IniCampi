58563c584e6f19122441f434f589dc4214f018e2
Rodrigo Costa <ianrodrigo25@gmail.com>
2024-11-09 18:45:27 -0300

    Update

--- user_account/forms.py
+++ user_account/forms.py
@@ -4,4 +4,4 @@ from .models import Profile
 class ProfileForm(forms.ModelForm):
     class Meta:
         models = Profile
-        fields = ['summary', 'profile_image', 'profile_background_image']
+        fields = ['user.username', 'summary', 'profile_image', 'profile_background_image']
--- user_account/urls.py
+++ user_account/urls.py
@@ -5,3 +5,4 @@ urlpatterns = [
     path('profile/', views.profile_view, name='profile_view'),
     path('profile/edit/', views.edit_profile, name='edit_profile'),
 ]
+ 
