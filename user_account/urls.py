63e5ec219851547a148378c9fdd55bb27697c2c2
Daniel Halos <82227817+Dhalos20@users.noreply.github.com>
2024-11-11 20:22:42 -0300

    Update perfil #1

--- user_account/urls.py
+++ user_account/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from feed.views import ProjectListView
 from . import views
 
 urlpatterns = [
     path('profile/', views.profile_view, name='profile_view'),
     path('profile/edit/', views.edit_profile, name='edit_profile'),
-    
+    path('', ProjectListView.as_view(), name='home'),
+    path('feed/', include('feed.urls')),
+
 ]
  
