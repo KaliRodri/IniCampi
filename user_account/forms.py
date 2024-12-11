6a706701453f246f45d6952d8d3f90b5aaea3172
Daniel Halos <82227817+Dhalos20@users.noreply.github.com>
2024-12-11 20:55:52 -0300

    edit_profile/css

--- user_account/forms.py
+++ user_account/forms.py
@@ -3,32 +3,33 @@ from feed.models import Profile, Skill
 
 class ProfileForm(forms.ModelForm):
     hard_skills = forms.ModelMultipleChoiceField(
-        queryset=Skill.objects.all(),
-        widget=forms.CheckboxSelectMultiple,
-        required=False
-    )
+    queryset=Skill.objects.all(),
+    widget=forms.CheckboxSelectMultiple(attrs={'class': 'checkbox-group'}),
+    required=False
+)
     
-    # Limite para o campo 'summary'
     summary = forms.CharField(
-        widget=forms.Textarea(attrs={'maxlength': '170'}),  # Limita a 100 caracteres no frontend
-        max_length=170,  # Limita a 100 caracteres no backend
+        widget=forms.Textarea(attrs={
+            'maxlength': '170',
+            'placeholder': 'Escreva um resumo...',
+            'class': 'form-control'
+        }),
+        max_length=170,
         required=False
     )
 
     class Meta:
         model = Profile
         fields = ['summary', 'hard_skills']
-        widgets = {
-            'hard_skills': forms.CheckboxSelectMultiple,
-        }
 
     def clean_hard_skills(self):
-        hard_skills = self.cleaned_data.get('hard_skills')
-        
-        # Validando a quantidade de hard skills selecionadas
+        hard_skills = self.cleaned_data.get('hard_skills', None)
+
+        if not hard_skills:
+            raise forms.ValidationError("Você precisa selecionar pelo menos 6 habilidades.")
         if len(hard_skills) < 6:
             raise forms.ValidationError("Você precisa selecionar pelo menos 6 habilidades.")
-        if len(hard_skills) > 6:
-            raise forms.ValidationError("Você pode selecionar no máximo 6 habilidades.")
+        if len(hard_skills) > 10:
+            raise forms.ValidationError("Você pode selecionar no máximo 10 habilidades.")
         
         return hard_skills
